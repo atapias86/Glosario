29f914b234f6404ce3e040e6bee62f47b6807c88
atapias86 <atapias86@misena.edu.co>
2022-09-30 11:44:31 -0500

    Map

--- Js/main.js
+++ Js/main.js
@@ -1,22 +1,10 @@
-addEventListener("DOMContentLoaded",(e)=>{
-    let from = document.querySelector("#form");
-    const list = [0,1,2,3,4,5,6,7,8,9,10,11,12];
+addEventListener("DOMContentLoaded", (e) => {
+    let btn = document.querySelector("#btn_inicio");
 
-    from.addEventListener("submit",(e)=>{
+    btn.addEventListener("click", (e) => {
         e.preventDefault();
-        data = Object.fromEntries(new FormData(e.target));
-        let num = Number(data.numero_mult);
-        document.querySelector("#dataTable_oprac").innerHTML ="";
-        from.reset();
-        for(let multp of list){
-            let resul = num * multp;
-            let plantilla = `
-            <tr>
-                <td>${num}</td>
-                <td>${multp}</td>
-                <td>${resul}</td>
-            </tr>`;
-        document.querySelector("#dataTable_oprac").insertAdjacentHTML("beforeend", plantilla);
-        }
-    })    
+        const list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        const map = list.map(x => x * 2);
+        document.querySelector("#resultado").insertAdjacentHTML("beforeend", "<br></br>" + map);
+    })
 })
--- README.md
+++ README.md
@@ -53,3 +53,20 @@ addEventListener("DOMContentLoaded",(e)=>{
     })    
 })
 ```
+
+## Map
+
+Sirve para realizar operaciones matemáticas a  interacciones 
+
+```js
+addEventListener("DOMContentLoaded", (e) => {
+  let btn = document.querySelector("#btn_inicio");
+  btn.addEventListener("click", (e) => {
+​    e.preventDefault();
+​    const list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+​    const map = list.map(x => x * 2);
+​    document.querySelector("#resultado").insertAdjacentHTML("beforeend", "<br></br>" + map);
+  })
+})
+```
+
